35d7cafc7a1004fc5a1d28bf3da07777790767f5
Bende <Bende@DESKTOP-7FUSUOA>
2023-05-04 16:16:47 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using BirthdayCard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult SubmissionForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmissionForm(CardDetails cardDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SentCard", cardDetails);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CardDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class CardDetails
+    {
+        [Required(ErrorMessage = "Please enter who the card is to")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter who the card is from")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message for the recipient")]
+        public string Message { get; set; }
+    }
+}
